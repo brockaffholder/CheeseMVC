68c31a1cddfaebcb9e40a85021ec4cff3b380bf5
Ben Clark <brclark15@gmail.com>
2019-12-18 13:22:23 -0600

    Use Cheese Class

--- CheeseMVC/Controllers/CheeseController.cs
+++ CheeseMVC/Controllers/CheeseController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using CheeseMVC.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -7,14 +8,38 @@ namespace CheeseMVC.Controllers
 {
     public class CheeseController : Controller
     {
-        private static Dictionary<string, string> Cheeses = new Dictionary<string, string>();
+        private static List<Cheese> Cheeses = new List<Cheese>();
 
         // GET: /<controller>/
+        // /Cheese/Index -- Get request
         public IActionResult Index()
         {
             ViewBag.cheeses = Cheeses;
 
             return View();
+        }
+
+        [Route("/Cheese")]
+        [Route("/Cheese/Index")]
+        [HttpPost]
+        public IActionResult RemoveCheese(string[] cheese_remove)
+        {
+            // Loop through the cheeses to remove from the HTML Form
+            foreach(string ch in cheese_remove)
+            {
+                // Loop through the Cheeses list to find the object
+                // corresponding to the cheese to remove
+                foreach(Cheese cheeseObject in Cheeses)
+                {
+                    if (ch.Equals(cheeseObject.Name))
+                    {
+                        Cheeses.Remove(cheeseObject);
+                        break;
+                    }
+                }
+            }
+
+            return Redirect("/Cheese/Index");
         }
 
         public IActionResult Add()
@@ -26,7 +51,8 @@ namespace CheeseMVC.Controllers
         [HttpPost]
         public IActionResult NewCheese(string name, string description)
         {
-            Cheeses.Add(name, description);
+            Cheese ch = new Cheese(name, description);
+            Cheeses.Add(ch);
 
             return Redirect("/Cheese");
         }
---
+++ CheeseMVC/Models/Cheese.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+namespace CheeseMVC.Models
+{
+    public class Cheese
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<string> Wines { get; set; }
+
+        public Cheese(string name, string description, List<string> wines)
+        {
+            Name = name;
+            Description = description;
+            Wines = wines;
+        }
+
+        public Cheese(string name, string description) : this(name, description, null) { }
+
+        public void AddToWinelist(string wine)
+        {
+            Wines.Add(wine);
+        }
+    }
+}
