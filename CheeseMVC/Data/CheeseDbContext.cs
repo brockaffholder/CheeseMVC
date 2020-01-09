34e72f1317b2b2826dfdb6c01d62784ff59372c2
Brock Affholder <brock.affholder@gmail.com>
2020-01-09 10:12:11 -0600

    stage 1 cheese mvc persistent

---
+++ CheeseMVC/Data/CheeseDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CheeseMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CheeseMVC.Data
+{
+    public class CheeseDbContext : DbContext
+    {
+        public DbSet<Cheese> Cheeses { get; set; }
+        public DbSet<CheeseCategory> Categories { get; set; }
+
+        public CheeseDbContext(DbContextOptions<CheeseDbContext> options)
+            : base(options)
+        { }
+    }
+}
